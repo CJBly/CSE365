b00b5c2b31365535d56b296dd8afd86a70ce31c8
CJBly <cjbly@asu.edu>
2025-08-27 16:40:15 -0700

    Create tls1.py

---
+++ tls1.py
@@ -0,0 +1,11 @@
+import sys
+import string
+import random
+import pathlib
+import base64
+import json
+
+
+from Crypto.Cipher import AES
+from Crypto.Hash.SHA256 import SHA256Hash
+from Crypto.PublicKey import RSA
